c81ad6e37262e6ff5ddf27dd4c710533fbf7c58d
Glauber Brennon <glauberbrennon@gmail.com>
2024-11-07 22:08:20 -0300

    Singleton is ready

--- src/design_patterns/creational/singleton.py
+++ src/design_patterns/creational/singleton.py
@@ -1,7 +1,31 @@
-class Singleton:
-    _instance = None
+from typing import Any, Optional
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
+class Singleton:
+    __instance: Optional['Singleton'] = None
+    __value: Any
+    __initialized: bool = False
+
+    def __new__(
+        cls,
+        *args: tuple[Any, ...],
+    ) -> 'Singleton':
+        if cls.__instance is None:
+            cls.__instance = super(Singleton, cls).__new__(cls)
+
+            if args:
+                cls.__value = args[0]
+        return cls.__instance
+
+    def __init__(self, value: Any) -> None:
+        if not self.__initialized:
+            self.__value = value
+            self.__initialized = True
+
+
+    @property
+    def value(self) -> Any:
+        return self.__value
+
+    @value.setter
+    def value(self, new_value: Any) -> None:
+        self.__value = new_value
--- tests/design_patterns/creational/test_singleton.py
+++ tests/design_patterns/creational/test_singleton.py
@@ -2,6 +2,7 @@ from src.design_patterns.creational.singleton import Singleton
 
 class TestSingleton:
     def test_singleton(self):
-        singleton1 = Singleton()
-        singleton2 = Singleton()
+        value = 'value'
+        singleton1 = Singleton(value,)
+        singleton2 = Singleton(value,)
         assert singleton1 is singleton2
