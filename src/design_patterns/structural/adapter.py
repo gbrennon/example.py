c6d5bde2bc6f57e14258ad9382f40d296b57e884
Glauber Brennon <glauberbrennon@gmail.com>
2024-11-11 20:59:34 -0300

    remove unecessary details

--- src/design_patterns/structural/adapter.py
+++ src/design_patterns/structural/adapter.py
@@ -1,37 +1,12 @@
 from abc import ABC, abstractmethod
 
+AdapterResult = dict[str, str]
+
 class Adapter(ABC):
     @abstractmethod
-    def adapt(self) -> str:
+    def adapt(self) -> AdapterResult:
         pass
-
-class Adaptee(ABC):
-    @abstractmethod
-    def specific_request(self) -> str:
-        pass
-
-class FooAdaptee:
-    def specific_request(self) -> str:
-        return "Foo Specific request"
-
-class BarAdaptee:
-    def specific_request(self) -> str:
-        return "Bar Specific request"
 
 class FooAdapter(Adapter):
-    __adaptee: FooAdaptee
-
-    def __init__(self, adaptee: FooAdaptee):
-        self.__adaptee = adaptee
-
-    def adapt(self) -> str:
-        return self.__adaptee.specific_request()
-
-class BarAdapter(Adapter):
-    __adaptee: BarAdaptee
-
-    def __init__(self, adaptee: BarAdaptee):
-        self.__adaptee = adaptee
-
-    def adapt(self) -> str:
-        return self.__adaptee.specific_request()
+    def adapt(self) -> AdapterResult:
+        return {"foo": "Foo Specific request"}
--- tests/design_patterns/structural/test_adapter.py
+++ tests/design_patterns/structural/test_adapter.py
@@ -1,28 +1,15 @@
 from src.design_patterns.structural.adapter import (
         FooAdapter,
-        FooAdaptee,
-        BarAdapter,
-        BarAdaptee
 )
 
 class TestFooAdapter:
     def test_adapt(self):
         # Arrange
-        adapter = FooAdapter(FooAdaptee())
+        adapter = FooAdapter()
 
         # Act
         result = adapter.adapt()
 
         # Assert
-        assert result == "Foo Specific request"
-
-class TestBarAdapter:
-    def test_adapt(self):
-        # Arrange
-        adapter = BarAdapter(BarAdaptee())
-
-        # Act
-        result = adapter.adapt()
-
-        # Assert
-        assert result == "Bar Specific request"
+        expected_result = {"foo": "Foo Specific request"}
+        assert result == expected_result
