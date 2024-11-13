df98385be6a2f6aaded8a30d30b1b8a02b80704b
Glauber Brennon <glauberbrennon@gmail.com>
2024-11-13 19:38:32 -0300

    app is now using .env

--- src/examples/flask/app.py
+++ src/examples/flask/app.py
@@ -1,29 +1,41 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
 
 from src.examples.flask.logger import Logger
 from src.examples.flask.services.weather_service import (
     create_weather_service,
 )
 
-app = Flask(__name__)
-logger = Logger()
-weather_api_key = "39bbc8668c644972816224326241211"
-weather_service = create_weather_service(weather_api_key)
+load_dotenv()
 
-@app.route('/')
-def hello_world():
-    text = 'Hello, World!'
-    logger.log(text)
+def create_app():
+    app = Flask(__name__)
+    logger = Logger()
+    weather_api_key = os.getenv("WEATHER_API_KEY")
 
-    return text
+    if weather_api_key is None:
+        raise Exception("WEATHER_API_KEY is not set")
 
-@app.route('/weather/<city>')
-def weather(city: str):
-    logger.log(f"Fetching weather for {city}")
-    weather = weather_service.fetch_weather(city)
+    weather_service = create_weather_service(weather_api_key)
 
-    return weather
+    @app.route('/')
+    def hello_world(): # type: ignore
+        text = 'Hello, World!'
+        logger.log(text)
+
+        return text
+
+    @app.route('/weather/<city>')
+    def weather(city: str): # type: ignore
+        logger.log(f"Fetching weather for {city}")
+        weather = weather_service.fetch_weather(city)
+
+        return weather
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    app = create_app()
+    app.run(debug=True)
