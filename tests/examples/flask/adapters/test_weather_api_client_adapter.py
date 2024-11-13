721a8f62b098124bfc06bd409df9f98a6a6a006f
Glauber Brennon <glauberbrennon@gmail.com>
2024-11-13 15:00:06 -0300

    added the failure scenario to tests

--- tests/examples/flask/adapters/test_weather_api_client_adapter.py
+++ tests/examples/flask/adapters/test_weather_api_client_adapter.py
@@ -1,6 +1,5 @@
 import pytest
 import requests_mock
-from requests.exceptions import HTTPError
 
 from src.examples.flask.adapters.weather_api_client_adapter import(
     WeatherAPIClientAdapter
@@ -51,3 +50,23 @@ class TestWeatherAPIClientAdapter:
             "temperature": 25.0
         }
         assert result == expected_result, "Result is different than expected"
+
+    def test_fetch_weather_failure(
+        self,
+        requests_mock: requests_mock.Mocker,
+        weather_adapter: WeatherAPIClientAdapter
+    ):
+        # Arrange
+        city = "Sao Paulo"
+        # Accessing private attribute to mock the URL
+        mock_url = weather_adapter._WeatherAPIClientAdapter__url # type: ignore
+        # Mocking the request
+        requests_mock.get(
+            f"{mock_url}?key={self.__fake_api_key}&q={city}&aqi=no",
+            status_code=500
+        )
+
+        # Act and Assert
+        with pytest.raises(Exception) as exception_info:
+            weather_adapter.fetch_weather(city)
+            exception_info.match(f"Failed to fetch weather data for {city}")
